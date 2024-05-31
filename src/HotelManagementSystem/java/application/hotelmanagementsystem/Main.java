56bab9238cc427e5b8255bf20985963b9419b07c
John_6670 <mayn2005m@gmail.com>
2024-05-31 21:56:27 +0330

    implemented welcome screen

--- src/HotelManagementSystem/java/application/hotelmanagementsystem/Main.java
+++ src/HotelManagementSystem/java/application/hotelmanagementsystem/Main.java
@@ -4,15 +4,17 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.kordamp.bootstrapfx.BootstrapFX;
 
 import java.io.IOException;
 
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("login-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("welcome-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 800, 500);
+        scene.getStylesheets().add(BootstrapFX.bootstrapFXStylesheet());
+        stage.setTitle("Welcome Page");
         stage.setScene(scene);
         stage.show();
     }
---
+++ src/HotelManagementSystem/java/application/hotelmanagementsystem/WelcomeController.java
@@ -0,0 +1,4 @@
+package application.hotelmanagementsystem;
+
+public class WelcomeController {
+}
