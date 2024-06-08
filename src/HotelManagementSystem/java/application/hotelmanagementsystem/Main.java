e2ae7c454025dc39d65ff4c68322badf16ba8824
John_6670 <mayn2005m@gmail.com>
2024-06-08 13:01:44 +0330

    added some improvements to buttons

--- src/HotelManagementSystem/java/application/hotelmanagementsystem/Main.java
+++ src/HotelManagementSystem/java/application/hotelmanagementsystem/Main.java
@@ -2,13 +2,16 @@ package application.hotelmanagementsystem;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.JavaFXBuilderFactory;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import org.kordamp.bootstrapfx.BootstrapFX;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class Main extends Application {
     public static double x, y;
@@ -30,6 +33,7 @@ public class Main extends Application {
         stage.setScene(scene);
         stage.setX(380);
         stage.setY(200);
+        stage.getIcons().add(new Image(Objects.requireNonNull(Main.class.getResourceAsStream("img/logo.png"))));
 
         root.setOnMousePressed(event -> {
             xxx = event.getSceneX();
