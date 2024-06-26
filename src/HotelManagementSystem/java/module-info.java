3fbde9d763e7f9e6795922b2dd5ac448c76a65d5
John <john2005mm@gmail.com>
2024-06-26 12:40:15 +0330

    small changes

--- src/HotelManagementSystem/java/module-info.java
+++ src/HotelManagementSystem/java/module-info.java
@@ -2,7 +2,6 @@ module application {
     requires javafx.controls;
     requires javafx.fxml;
 
-    requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
